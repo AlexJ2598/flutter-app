0e0ea8fb80f4d0b8c47abf37075d73045d84a801
AlexJ2598 <67809818+AlexJ2598@users.noreply.github.com>
2024-05-22 23:30:47 -0600

    Tipos dynamicos
    Se muestra el uso de las variables tipo dinamico

---
+++ dynamic.dart
@@ -0,0 +1,11 @@
+void main() {
+  //dynamic == null
+  //se usa cuando recibimos datos externos y vamos a mapearlos. No es recomendable abusar de ellos.
+  dynamic errorMessage = 'Error del sistema';
+  //Al ser dinamico puede recibir cualquier valor.
+  print("primer valor de la variable dinamica: ");
+  print("$errorMessage");
+  errorMessage = [1, 2, 3, 4];
+  print("Ahora es una matriz: ");
+  print("$errorMessage");
+}
